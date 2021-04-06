2f067b799107b4b26f563e2cb7e4539cd70e0e79
Tung Phan Van (tungpv-0974) <phan.van.tung@sun-asterisk.com>
2021-04-06 13:55:35 +0700

    change @JsonNaming to SnakeCase

--- src/main/java/com/tungpv/wallet/dto/response/CreateWalletResponseDto.java
+++ src/main/java/com/tungpv/wallet/dto/response/CreateWalletResponseDto.java
@@ -1,5 +1,7 @@
 package com.tungpv.wallet.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class CreateWalletResponseDto {
     private String address;
     private Long balance;
